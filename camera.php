b829e5bb4b50bf309e4b5c690a168699177fbaac
Katarina-Slotova <katarina.slotova@gmail.com>
2022-09-27 20:41:52 +0300

    Photos with stickers from webcam working, preview overlapping inputs.

--- camera.php
+++ camera.php
@@ -12,55 +12,60 @@ require_once('header.php');
 			<p class="has-text-centered message is-danger"><?php echo $_GET['error_message']?></p>
 		<?php } ?>
 		<div class="camera">
-			<div class="camera-img">
-				<form runat="server" action="create_camera_post.php" method="POST" enctype="multipart/form-data" class="camera-form">
-					<div>
-						<p class="sticker-description">1. Choose a sticker to jazz up your awesome photo!</p>
-						<div class="stickers-box">
-							<div class="stickers-container">
-								<img name="bee" class="sticker" src="assets/stickers/bee.png" alt="bee-sticker" id="sticker1">
-								<img class="sticker" src="assets/stickers/kitten.png" alt="kitten-sticker" id="sticker2">
-								<img class="sticker" src="assets/stickers/monster.png" alt="monster-sticker" id="sticker3">
-								<img class="sticker" src="assets/stickers/so-hot.png" alt="hot-sticker" id="sticker4">
-								<img class="sticker" src="assets/stickers/unicorn.png" alt="unicorn-sticker" id="sticker5">
-								<img class="sticker" src="assets/stickers/watermelon.png" alt="watermalon-sticker" id="sticker6">
+			<div class="camera-img" style="display:flex;">
+				<div style="width:90%;">
+					<form action="create_camera_post.php" method="POST" enctype="multipart/form-data" class="camera-form">
+						<div>
+							<p class="sticker-description">1. Choose a sticker to jazz up your awesome photo!</p>
+							<div class="stickers-box">
+								<div class="stickers-container">
+									<img class="sticker" src="assets/stickers/bee.png" alt="bee-sticker" id="sticker1">
+									<img class="sticker" src="assets/stickers/kitten.png" alt="kitten-sticker" id="sticker2">
+									<img class="sticker" src="assets/stickers/monster.png" alt="monster-sticker" id="sticker3">
+									<img class="sticker" src="assets/stickers/so-hot.png" alt="hot-sticker" id="sticker4">
+									<img class="sticker" src="assets/stickers/unicorn.png" alt="unicorn-sticker" id="sticker5">
+									<img class="sticker" src="assets/stickers/watermelon.png" alt="watermelon-sticker" id="sticker6">
+								</div>
 							</div>
 						</div>
-					</div>
-					<p style="margin-top: 30px;" class="sticker-description">2. Take an awesome awesome photo!</p>
-					<div>
+						<p style="margin-top: 30px;" class="sticker-description">2. Take an awesome awesome photo!</p>
 						<button class="capture-btn" id="start-camera">Start Camera</button>
-						<div class="canvas-container">
-							<video id="video" width="700" height="500" autoplay></video>
+						<div>
+							<video class="is-hidden" id="video" width="700" height="500" autoplay></video>
 						</div>
 						<button class="capture-btn" id="click-photo">Capture Photo</button>
-						<p style="margin-top: 30px;" class="sticker-description">The photo you have taken:</p>
-						<div class="canvas-container">
-							<canvas width="700" height="500" id="canvas"></canvas>
-							<input type="hidden" id="webcam-file" value="" name="webcam_file">
+						<p style="margin-top: 30px;" class="sticker-description">The awesome photo you have taken:</p>
+						<div style="position:relative;">
+							<div style="position:absolute; ">
+								<canvas class="is-hidden" width="700" height="500" id="canvas"></canvas>
+								<input type="hidden" id="webcam-file" value="" name="webcam_file">
+							</div>
+							<!-- <p style="margin-top: 30px;" class="sticker-description">The stickers you have chosen:</p> -->
+							<div style="position:absolute; ">
+								<canvas class="is-hidden" width="700" height="500" id="stickers_canvas"></canvas>
+								<input type="hidden" id="sticker-canvas" value="" name="sticker-canvas">
+								<input type="hidden" id="sticker1_path" value="" name="sticker1_path">
+								<input type="hidden" id="sticker2_path" value="" name="sticker2_path">
+								<input type="hidden" id="sticker3_path" value="" name="sticker3_path">
+								<input type="hidden" id="sticker4_path" value="" name="sticker4_path">
+								<input type="hidden" id="sticker5_path" value="" name="sticker5_path">
+								<input type="hidden" id="sticker6_path" value="" name="sticker6_path">
+							</div>
 						</div>
-						<p style="margin-top: 30px;" class="sticker-description">The stickers you have chosen:</p>
-						<div class="canvas-container">
-							<canvas width="700" height="500" id="stickers_canvas"></canvas>
-							<input type="hidden" id="sticker-canvas" value="" name="sticker-canvas">
-							<input type="hidden" id="sticker1_path" value="" name="sticker1_path">
-							<input type="hidden" id="sticker2_path" value="" name="sticker2_path">
-							<input type="hidden" id="sticker3_path" value="" name="sticker3_path">
-							<input type="hidden" id="sticker4_path" value="" name="sticker4_path">
-							<input type="hidden" id="sticker5_path" value="" name="sticker5_path">
-							<input type="hidden" id="sticker6_path" value="" name="sticker6_path">
+						<div class="control">
+							<input type="text" class="my-input input" name="caption" placeholder="Write a caption here" required>
 						</div>
-					</div>
-					<div class="control">
-						<input type="text" class="my-input input" name="caption" placeholder="Write a caption here" required>
-					</div>
-					<div class="control">
-						<input type="text" class="my-input input" name="hashtags" placeholder="Add hastags here" required>
-					</div>
-					<div>
-						<button type="submit" class="upload-btn" name="webcam_img_btn">Publish</button>
-					</div>
-				</form>
+						<div class="control" >
+							<input type="text" class="my-input input" name="hashtags" placeholder="Add hastags here" required>
+						</div>
+						<div>
+							<button type="submit" class="upload-btn" name="webcam_img_btn">Publish</button>
+						</div>
+					</form>
+				</div>
+				<div>
+					<p>here is div</p>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -93,6 +98,7 @@ require_once('header.php');
 		
 		camera_button.addEventListener('click', async function() {
 			let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+			video.classList.remove("is-hidden");
 			video.srcObject = stream;
 		});
 		
@@ -103,27 +109,27 @@ require_once('header.php');
 
 			switch (sticker){
 				case 'sticker1':
-					stickers_ctx.drawImage(Selectedsticker, 30, 40, Selectedsticker.width * 1.20, Selectedsticker.height * 1.20);
+					stickers_ctx.drawImage(Selectedsticker, 30, 40, Selectedsticker.width * 0.8, Selectedsticker.height * 0.8);
 					sticker1.value = "assets/stickers/bee.png"
 					break;
 				case 'sticker2':
-					stickers_ctx.drawImage(Selectedsticker, 300, 40, Selectedsticker.width * 1.20, Selectedsticker.height * 1.20);
+					stickers_ctx.drawImage(Selectedsticker, 300, 40, Selectedsticker.width * 0.8, Selectedsticker.height * 0.8);
 					sticker2.value = "assets/stickers/kitten.png"
 					break;
 				case 'sticker3':
-					stickers_ctx.drawImage(Selectedsticker, 150, 200, Selectedsticker.width * 1.20, Selectedsticker.height * 1.20);
+					stickers_ctx.drawImage(Selectedsticker, 150, 220, Selectedsticker.width * 0.8, Selectedsticker.height * 0.8);
 					sticker3.value = "assets/stickers/monster.png"
 					break;
 				case 'sticker4':
-					stickers_ctx.drawImage(Selectedsticker, 150, 80, Selectedsticker.width * 1.20, Selectedsticker.height * 1.20);
+					stickers_ctx.drawImage(Selectedsticker, 170, 80, Selectedsticker.width * 0.8, Selectedsticker.height * 0.8);
 					sticker4.value = "assets/stickers/so-hot.png"
 					break;
 				case 'sticker5':
-					stickers_ctx.drawImage(Selectedsticker, 30, 200, Selectedsticker.width * 1.20, Selectedsticker.height * 1.20);
+					stickers_ctx.drawImage(Selectedsticker, 30, 200, Selectedsticker.width * 0.8, Selectedsticker.height * 0.8);
 					sticker5.value = "assets/stickers/unicorn.png"
 					break;
 				case 'sticker6':
-					stickers_ctx.drawImage(Selectedsticker, 300, 200, Selectedsticker.width * 1.20, Selectedsticker.height * 1.20);
+					stickers_ctx.drawImage(Selectedsticker, 300, 200, Selectedsticker.width * 0.8, Selectedsticker.height * 0.8);
 					sticker6.value = "assets/stickers/watermelon.png"
 					break;
 			}
@@ -134,19 +140,13 @@ require_once('header.php');
 
 		capture_button.addEventListener('click', function() {
 			let canvas = document.getElementById("canvas");
-			
+			canvas.classList.remove("is-hidden");
+			stickers_canvas.classList.remove("is-hidden");
 			let ctx = canvas.getContext("2d");
-
 			ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-			
 			let canvasUrl = canvas.toDataURL();
-			
-
 			let finalImage = document.getElementById("webcam-file");
-			
-
 			finalImage.value = canvasUrl;
-			
 		});
 
 
