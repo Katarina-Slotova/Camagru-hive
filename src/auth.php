8ad71206471e0ed28709bf6ae4c5586ebc1d62af
Katarina Slotova <kslotova@c2r2p11.hive.fi>
2022-10-06 09:20:19 +0300

    Add PDOException to auth file.

--- src/auth.php
+++ src/auth.php
@@ -6,16 +6,22 @@ $email = $_GET['email'];
 
 function activate_user(string $email): bool
 {
-	$conn = connect_db();
-	$sql = 'UPDATE users
-			SET active = 1,
-				activated_at = CURRENT_TIMESTAMP
-			WHERE email=:email';
+	try{
+		$conn = connect_db();
+		$sql = 'UPDATE users
+				SET active = 1,
+					activated_at = CURRENT_TIMESTAMP
+				WHERE email=:email';
+	
+		$stmt = $conn->prepare($sql);
+		$stmt->bindValue(':email', $email, PDO::PARAM_STR);
+	
+		$stmt->execute();
 
-	$stmt = $conn->prepare($sql);
-	$stmt->bindValue(':email', $email, PDO::PARAM_STR);
-
-	$stmt->execute();
+	} catch (PDOException $error) {
+		echo $error->getMessage(); 
+		exit;
+	}
 
 	return $stmt->rowCount();
 }
